da1e5f2c8af58ae67216eaf36586256fe04641dc
satrirattt <s6606021620198@email.kmutnb.ac.th>
2024-12-05 19:30:41 +0700

    L2

--- Lecture02/functionequa.js
+++ Lecture02/functionequa.js
@@ -1,4 +1,7 @@
-function findPerimeter(length,width) {
-    return (length + width) * 2;
+function equation(str) {
+    return eval(str);
     
 }
+console.log(equation("1+1"));
+console.log(equation("7*4-2"));
+console.log(equation("1+1+1+1+1"));
