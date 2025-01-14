6f3a6750c190e5a17fdf7a95b61a9eca5b4cc563
satrirattt <s6606021620198@email.kmutnb.ac.th>
2025-01-14 21:14:44 +0700

    L5

---
+++ Lecture05/ASYNC-AWAIT/asyncawait-2.js
@@ -0,0 +1,23 @@
+function promiseTimeout(ms) {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, ms);
+    });
+    
+}
+
+async function longRunningOperation() {
+    //logic
+    return 42;
+}
+
+async function run() {
+    //logic
+    console.log("Start!!");
+    await promiseTimeout(2000);
+    //try take of await and compare
+    const response = await longRunningOperation();
+    console.log(response);
+    console.log("Stop!!");    
+}
+
+run();
---
+++ Lecture05/ASYNC-AWAIT/asynccatch.js
@@ -0,0 +1,24 @@
+function yayOrNay() {
+    return new Promise((resolve, reject) => {
+        const val = Math.round(Math.random() * 1);
+        // 0 or 1, at random
+        val ? resolve("Lucky!!") : reject("Nope 😠");
+    });
+}
+
+async function msg() {
+    try {
+        const msg = await yayOrNay();
+        console.log(msg);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+msg(); // Luckky!!
+msg(); // Luckky!!
+msg(); // Luckky!!
+msg(); // Nope 😠
+msg(); // Luckky!!
+msg(); // Nope 😠
+msg(); // Nope 😠
